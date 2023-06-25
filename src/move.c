ec1f13d15e35b1a44b48b36f878dbec9251c17a1
Tim <horochowiktim@pm.me>
2023-06-25 21:19:00 +0200

    cleaned up code

--- src/move.c
+++ src/move.c
@@ -1,24 +1,13 @@
 int move(char s[], int position, int length) {
-    int b = 0;
-    if (s[position] == ' ') {
-        for (int j = position; j <= length; ++j) {
-            s[j] = s[j+1];
-        }
-        length -= 1;
-        b = 1;
-    }
-    if (b == 0) {
+    if (s[position] != ' ') {
         for (int i = length; i >= position; --i) {
             s[i+2] = s[i];
         }
         s[position] = '-';
         s[position+1] = '\n';
-        return length+2;
+        length += 2;
     } else {
-        for (int i = length; i >= position; --i) {
-            s[i+1] = s[i];
-        }
         s[position] = '\n';
-        return length+1;
     }
+    return length;
 }
